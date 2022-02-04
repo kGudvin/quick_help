d885c8cc1a068b060170e5251290813a8402669c
Khohikov Akhmad <ahmad@hojiko.ru>
2022-02-04 16:51:02 +0300

    add button

--- client/src/components/Avatar/Avatar.js
+++ client/src/components/Avatar/Avatar.js
@@ -1,5 +1,6 @@
 import style from './style.module.css'
 import default_avatar from './default_avatar.png'
+import { Link } from 'react-router-dom'
 
 
 const Avatar = () => {
@@ -8,10 +9,12 @@ const Avatar = () => {
 
 
     return (
+        <div className={`${style.av}`}>
+        <Link to="/signOut" className={`${style.nav_link}`}>SignOut</Link>
         <div className={`${style.avatar}`}>
             <img src={default_avatar} height={43} className={`${style.icon}`}/>
         </div>
-
+    </div>
     )
 }
 
